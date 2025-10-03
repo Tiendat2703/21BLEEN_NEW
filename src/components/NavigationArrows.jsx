7b882bbddb7002967ba0ba4a8778b68a495d759d
Thong <dn7110994@gmail.com>
2025-10-03 17:09:00 +0700

    update frontend

--- src/components/NavigationArrows.jsx
+++ src/components/NavigationArrows.jsx
@@ -51,7 +51,7 @@ function NavigationArrows() {
       {previousPage && (
         <div
           onClick={handleLeftClick}
-          className="fixed left-0 top-0 bottom-0 w-1/6 z-10 cursor-pointer hover:bg-black hover:bg-opacity-5 transition-all"
+          className="fixed left-0 top-0 bottom-0 w-1/6 z-10 cursor-pointer hover:bg-opacity-5 transition-all"
           aria-label="Previous page"
         />
       )}
@@ -60,7 +60,7 @@ function NavigationArrows() {
       {nextPage && (
         <div
           onClick={handleRightClick}
-          className="fixed right-0 top-0 bottom-0 w-1/6 z-10 cursor-pointer hover:bg-black hover:bg-opacity-5 transition-all"
+          className="fixed right-0 top-0 bottom-0 w-1/6 z-10 cursor-pointer hover:bg-opacity-5 transition-all"
           aria-label="Next page"
         />
       )}
